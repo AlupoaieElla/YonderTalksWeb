8dd2a89df0d433cd88a24ab8d6c6550e25d41adc
Elena Alupoaie <ElenaA@yonder.local>
2017-04-06 15:54:08 +0300

    Created the page for desktop display

---
+++ js/script.js
@@ -0,0 +1,85 @@
+/* scrolling down smooth */
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    // Scroll efect - HAYA
+    $('.container').each(function() {
+        var $bgobj = $(this);
+
+        $(window).scroll(function(ev) {
+            if ($bgobj.isOnScreen()) {
+                var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+
+                // Put together our final background position
+                var coords = '50% ' + yPos + 'px';
+
+                // Move the background
+                $bgobj.css({ backgroundPosition: coords });
+            }
+        })
+    })
+
+    $(document).ready(function() {
+        $("#cf_onclick").click(function() {
+            $("#cf2 img.top").toggleClass("transparent");
+        });
+    });
+
+    window.onload = function() {
+
+        var background_characters = {
+            badea: "#7D8B99",
+            hagiu: "#8DC63F",
+            paul: "#FFCF4A",
+            pava: "#E84D3D",
+            grigi: "#8D44AD"
+        };
+        var talk = document.getElementById("talk");
+        var speakers__container = document.getElementById("speakers__container");
+        speakers__container.addEventListener("click", function(e) {
+            var img = e.target.id;
+            if (background_characters.hasOwnProperty(img)) {
+                console.log("Found it!" + img);
+                talk.style.backgroundColor = background_characters[img];
+            }
+        });
+
+
+        /*fade in out logo and description in intro*/
+        function swap() {
+            $('.intro__description').fadeIn(2000);
+            $('.logo').addClass("fadeout");
+        }
+        setTimeout(swap, 1000);
+    };
+});
+
+/* parallax*/
+$.fn.isOnScreen = function() {
+
+    var win = $(window);
+
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
